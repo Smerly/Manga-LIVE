ddf6f041148a23f1b8334eb8a9b52833477ffb8d
Ernest Cheung <ErnestCheung02@gmail.com>
2020-12-08 18:14:45 -0800

    update

--- src/Hero.js
+++ src/Hero.js
@@ -17,21 +17,6 @@ function Hero() {
 					</div>
 				</div>
 			</section>
-
-			{/* <Modal.Dialog>
-				<Modal.Header closeButton>
-					<Modal.Title>Modal title</Modal.Title>
-				</Modal.Header>
-
-				<Modal.Body>
-					<p>Modal body text goes here.</p>
-				</Modal.Body>
-
-				<Modal.Footer>
-					<Button variant="secondary">Close</Button>
-					<Button variant="primary">Save changes</Button>
-				</Modal.Footer>
-			</Modal.Dialog> */}
 		</div>
 	);
 }
