5f67b6a89e32a43094f0d36551ecbdfe3bdad7f2
Ernest Cheung <ErnestCheung02@gmail.com>
2020-12-11 14:07:54 -0800

    update for fun

--- src/page1/Hero.js
+++ src/page1/Hero.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Modal from 'react-bootstrap/Modal';
-import Cover from '../images/CoverForManga.png';
+// import Cover from '../images/CoverForManga.png';
 import '../App.css';
+import Carouselimg from './Carousel';
 
 function Hero() {
 	return (
@@ -10,11 +11,12 @@ function Hero() {
 				<div className="jumbotron jumbotron-fluid herocustom p-5 d-flex justify-content-center">
 					<div className="container d-flex justify-content-center">
 						<div className="row">
-							<img
+							{/* <img
 								src={Cover}
 								className="img-fluid mx-auto images-hero"
 								alt="Example of Successful Manga"
-							/>
+							/> */}
+							<Carouselimg />
 						</div>
 					</div>
 				</div>
