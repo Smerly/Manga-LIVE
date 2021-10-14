c9bfe857657871af2c0ba42af6d9104f757661d5
Ernest Cheung <ernestcheung02@gmail.com>
2021-10-13 18:25:28 -0700

    Styled displaying from DB

--- src/MangaList.js
+++ src/MangaList.js
@@ -13,25 +13,64 @@ function MangaList() {
 		dispatch(loadManga());
 	}, []);
 
-	// const [search, setSearch] = useState('');
+	const [search, setSearch] = useState('');
 	const mangas = useSelector((state) => state.mangas);
-	console.log(mangas);
 
-	// firestore.initializeApp(firebaseConfig);
-	// if (true) {
-	// 	writeDailySpecial();
-	// }
-	// console.log(writeDailySpecial);
+	const MangaList = mangas
+		.filter((val) => {
+			if (search == '') {
+				return val;
+			} else if (val.title.toLowerCase().includes(search.toLowerCase())) {
+				return val;
+			}
+		})
+		.map((mangaLayer2, index1) => {
+			return (
+				<li key={index1} style={{ listStyleType: 'none' }}>
+					<div className="card m-2 mb-3 listbox">
+						<button
+							className="column p-2"
+							style={{
+								width: '15rem',
+								height: '15rem',
+								background: 'none',
+								border: 'none',
+							}}
+						>
+							<div className="col-sm mb-2" style={{ color: 'black' }}>
+								<h3>{mangaLayer2[1].title}</h3>
+							</div>
+							<div className="col-sm mb-2" style={{ color: 'black' }}>
+								By: {mangaLayer2[1].author}
+							</div>
+							<div className="col-sm mb-2" style={{ color: 'black' }}>
+								Art by: {mangaLayer2[1].artist}
+							</div>
+							{/* <div className="col-sm mb-2" style={{ color: 'black' }}>
+								Chapters: {mangaLayer2[1].pages}
+							</div> */}
+						</button>
+					</div>
+				</li>
+			);
+		});
+
 	return (
 		<div>
-			<div>
-				{mangas.map((mangaLayer2, index1) => {
-					return (
-						<div key={index1}>
-							<div>{mangaLayer2[1].title}</div>
-						</div>
-					);
-				})}
+			<div className="container bg-light" style={{ minHeight: 700 }}>
+				<form className="form-inline mt-5 mb-5" action="/action_page.php">
+					{/* <input
+						type="search"
+						placeholder="Search.."
+						className="mr-sm-2 pl-3 my-2 ml-3 search3 mt-5"
+						onChange={(e) => {
+							setSearch(e.target.value);
+						}}
+					/> */}
+				</form>
+				<div className="row justify-content-center align-items-center">
+					{MangaList}
+				</div>
 			</div>
 		</div>
 	);
